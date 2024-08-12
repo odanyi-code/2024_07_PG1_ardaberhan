4f6e70c44124856f4fdb0cefb36955479633494b
odanyi-code <ardabd9@gmail.com>
2024-08-12 19:20:51 +0300

    Add files via upload

---
+++ src/odevler/bolum01/bolum01/Q01.java
@@ -0,0 +1,9 @@
+package odevler.bolum01;
+
+public class Q01 {
+    public static void main(String[] args) {
+        System.out.println( "Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programing is fun");
+    }
+}
